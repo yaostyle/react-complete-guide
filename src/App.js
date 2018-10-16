c5e37b2901545cd6902cef23602f26598d92eef3
Chris <yaostyle@gmail.com>
2018-10-16 10:40:05 -0700

    JSX restrictions and etc.

--- src/App.js
+++ src/App.js
@@ -5,9 +5,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      <h1>Hi I'm a React App</h1>
+        <h1>Hi I'm a React App</h1>
+        <p>This is really working!</p>
       </div>
     );
+
+    //return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
